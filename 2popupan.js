be5d040392f8102b74d5c7c0fd0dabbd0af08ea1
DanhBuon <49446108+DanhBuon@users.noreply.github.com>
2019-11-04 14:17:45 +0700

    Update 2popupan.js

--- 2popupan.js
+++ 2popupan.js
@@ -2,7 +2,7 @@
 	var url_popup = 'https://kesatngoctin.com/danh-muc/ke-tai-trong-nang';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'https://kesatngoctin.com/danh-muc/ke-de-hang';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
